46b303650c98c8a583a042d55f40bba7261f1af5
Manas Pradhan <manasmpradhan5@gmail.com>
2022-07-26 00:11:19 +0530

    changed incorrect class to error

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', e => {
     var pass2 = document.querySelector('#confirm-pass');
     if (pass1.value != pass2.value) {
         e.preventDefault();
-        pass1.classList.add('incorrect');
-        pass2.classList.add('incorrect');
+        pass1.classList.add('error');
+        pass2.classList.add('error');
     }
 })
